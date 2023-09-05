65fad5ce3b65d28019eff920ef9b40be50a75499
JorgeDesk <JorgeDesk@DESKTOP-I21G65G.lan>
2023-09-04 18:03:05 -0600

    Cambios guardado de Investigaciones

--- horusControl/src/main/java/mx/com/rmsh/horusControl/service/InvestigacionService.java
+++ horusControl/src/main/java/mx/com/rmsh/horusControl/service/InvestigacionService.java
@@ -2,7 +2,6 @@ package mx.com.rmsh.horusControl.service;
 
 import java.util.List;
 
-
 import mx.com.rmsh.horusControl.vo.Investigacion;
 import mx.com.rmsh.horusControl.vo.InvestigacionRequest;
 import mx.com.rmsh.horusControl.vo.ReporteRequest;
--- horusControl/src/main/java/mx/com/rmsh/horusControl/vo/InvestigacionLAMBDA.java
+++ horusControl/src/main/java/mx/com/rmsh/horusControl/vo/InvestigacionLAMBDA.java
@@ -5,45 +5,66 @@ import java.util.ArrayList;
 import org.springframework.web.servlet.function.ServerRequest.Headers;
 
 public class InvestigacionLAMBDA {
-	
-	public int statusCode;	
+
+	public int statusCode;
+	public Long created;
+	public String jsonBD;
 	public Body body;
+
 	public int getStatusCode() {
 		return statusCode;
 	}
+
 	public void setStatusCode(int statusCode) {
 		this.statusCode = statusCode;
 	}
+
 	public Body getBody() {
 		return body;
 	}
+
 	public void setBody(Body body) {
 		this.body = body;
 	}
+
+	public Long getCreated() {
+		return created;
+	}
+
+	public void setCreated(Long created) {
+		this.created = created;
+	}
+
+	public String getJsonBD() {
+		return jsonBD;
+	}
+
+	public void setJsonBD(String jsonBD) {
+		this.jsonBD = jsonBD;
+	}
+
 	@Override
 	public String toString() {
-		return "InvestigacionLAMBDA [statusCode=" + statusCode + ", body=" + body + "]";
+		return "InvestigacionLAMBDA [statusCode=" + statusCode + ", created=" + created + ", jsonBD=" + jsonBD
+				+ ", body=" + body + "]";
 	}
-	
-	
+
 }
 
 class Body {
-	
+
 	public ArrayList<Origen> origen;
 	public Integer nivel_riesgo;
 	public ArrayList<String> parametros_busqueda;
 	public ArrayList<String> fuentes;
-	
+
 	@Override
 	public String toString() {
 		return "Body [origen=" + origen + ", nivel_riesgo=" + nivel_riesgo + ", parametros_busqueda="
 				+ parametros_busqueda + ", fuentes=" + fuentes + "]";
 	}
-	
-	
-}
 
+}
 
 class Origen {
 	public String nombre;
@@ -53,13 +74,16 @@ class Origen {
 	public String summary;
 	public String fuente;
 	public String score;
+	public String description;
+	public String risk_level;
+
 	@Override
 	public String toString() {
 		return "Origen [nombre=" + nombre + ", url=" + url + ", resultado=" + resultado + ", texto=" + texto
-				+ ", summary=" + summary + ", fuente=" + fuente + ", score=" + score + "]";
+				+ ", summary=" + summary + ", fuente=" + fuente + ", score=" + score + ", description=" + description
+				+ ", risk_level=" + risk_level + "]";
 	}
-	
-	
+
 }
 
 class Root {
